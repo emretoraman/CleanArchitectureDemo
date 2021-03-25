a90033bbfad575b8271809d836682b837388b68f
Emre Toraman <emretoraman@gmail.com>
2021-03-25 13:26:37 +0200

    Implement CourseService, CourseRepository

---
+++ CleanArchitecture/CleanArchitecture.Infrastructure.Data/Repositories/CourseRepository.cs
@@ -0,0 +1,22 @@
+﻿using CleanArchitecture.Domain.Interfaces;
+using CleanArchitecture.Domain.Models;
+using CleanArchitecture.Infrastructure.Data.Context;
+using System.Collections.Generic;
+
+namespace CleanArchitecture.Infrastructure.Data.Repositories
+{
+    public class CourseRepository : ICourseRepository
+    {
+        private UniversityDbContext _context;
+
+        public CourseRepository(UniversityDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Course> GetCourses()
+        {
+            return _context.Courses;
+        }
+    }
+}
