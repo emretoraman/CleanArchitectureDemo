09815876cab295ffbf00e0da9356b3e97f6ea318
Emre Toraman <emretoraman@gmail.com>
2021-03-26 21:50:30 +0200

    Commands, CommandHandlers, Update CourseRepository, Clean up IoC Container

--- CleanArchitecture/CleanArchitecture.Infrastructure.Data/Repositories/CourseRepository.cs
+++ CleanArchitecture/CleanArchitecture.Infrastructure.Data/Repositories/CourseRepository.cs
@@ -18,5 +18,11 @@ namespace CleanArchitecture.Infrastructure.Data.Repositories
         {
             return _context.Courses;
         }
+
+        public void Add(Course course)
+        {
+            _context.Courses.Add(course);
+            _context.SaveChanges();
+        }
     }
 }
